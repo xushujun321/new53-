5490c401ba7b9294a3ca3a7252e099dc3a3630c7
xsj321 <1932806752@qq.com>
2020-08-23 08:36:23 +0800

    个人中心子页头部封装

---
+++ src/components/封装头部组件.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="top">
+    {{name1}}
+    <span class="iconfont iconjiantou2" @click="submit"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['name1'],
+  methods: {
+    submit() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.top {
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  position: relative;
+  .iconjiantou2 {
+    position: absolute;
+    left: 10px;
+    top: 0px;
+    font-size: 18px;
+  }
+}
+</style>
